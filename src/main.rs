ad7a477add7a2346d990d33c7935f648e7412890
David Wright <djwright3.14@gmail.com>
2022-11-11 10:16:55 -0500

    GUI added

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,79 @@
+use fltk::{app, button::Button, frame::Frame, prelude::*, window::Window, input::FloatInput, group::Flex};
 fn main() {
+    let app = app::App::default();
+    let mut wind = Window::default().with_size(500, 800).with_label("Quenching Temperatures");
+    let mut flex = Flex::default()
+        .with_size(240,800)
+        .with_pos(250,0)
+        .column();
+    let mut geometry_label = Frame::default().with_label("Geometry");
+    let mut thickness_input = FloatInput::default()
+        .with_label("Material Thickness [mm]");
+    thickness_input.set_value(&"28.0");
+    let mut pysics_label = Frame::default().with_label("Physics");
+    let mut initial_temp_input = FloatInput::default()
+        .with_label("Initial Material Temperature [C]");
+    initial_temp_input.set_value(&"280.0");
+    let mut fluid_temp_input = FloatInput::default()
+        .with_label("Fluid Temperature [C]");
+    fluid_temp_input.set_value(&"10.0");
+    let mut convective_input = FloatInput::default()
+        .with_label("Convective Coefficient [W/m^2*K]");
+    convective_input.set_value(&"2000.0");
+    let mut simulation_label = Frame::default().with_label("Simulation Settings");
+    let mut duration_input = FloatInput::default()
+        .with_label("Duration [Seconds]");
+    duration_input.set_value(&"90.0");
+    let mut timestep_input = FloatInput::default()
+        .with_label("Time Step [Seconds]");
+    timestep_input.set_value(&"0.0010");
+    let mut material_subdivs_input = FloatInput::default()
+        .with_label("Material Subdivisions");
+    material_subdivs_input.set_value(&"20");
+    let mut button_start = Button::default()
+        .with_label("Run Simulation");
+    let mut inner_temp_label = Frame::default().with_label("");
+    let mut outer_temp_label = Frame::default().with_label("");
+    flex.end();
+    wind.end();
+    wind.show();
+
     // Simulation Params
-    let subdiv_count = 10;
-    let thickness = 0.028; // meters
-    let temp_initial = 250.0; // Celcius
-    let material_k = 40.; // W/m*K
-    let specific_heat = 500.; // J/kg*K
-    let density = 7850.; // kg/m^3
-    let convective_h = 2000.; // W/m^2*K
-    let fluid_temp = 20.; // C
-    let total_duration = 60.; //seconds
-    let timestep = 0.1; // seconds
-    let dx = thickness/subdiv_count as f64;
-    let cell_mass = mass(density, dx);
-    let (_final_temps, outer_temps, inner_temps) = iterate(temp_initial, dx, material_k, specific_heat, cell_mass, fluid_temp, convective_h, subdiv_count, total_duration, timestep);
-    println!("Inner Wall Temperature: {:.3}",inner_temps[inner_temps.len()-1]);
-    println!("Outer Wall Temperature: {:.3}",outer_temps[outer_temps.len()-1]);
+    let mut subdiv_count = 10.;
+    let mut thickness = 0.028; // meters
+    let mut temp_initial = 250.0; // Celcius
+    let mut material_k = 40.; // W/m*K
+    let mut specific_heat = 500.; // J/kg*K
+    let mut density = 7850.; // kg/m^3
+    let mut convective_h = 2000.; // W/m^2*K
+    let mut fluid_temp = 20.; // C
+    let mut total_duration = 60.; //seconds
+    let mut timestep = 0.001; // seconds
+
+    /* Event handling */
+    button_start.set_callback(move |_|
+        run(&parse_float_input(&initial_temp_input),
+            &(parse_float_input(&thickness_input)/1000.),
+            &parse_float_input(&material_subdivs_input),
+            &material_k,
+            &specific_heat,
+            &density,
+            &parse_float_input(&fluid_temp_input),
+            &parse_float_input(&convective_input),
+            &parse_float_input(&duration_input),
+            &parse_float_input(&timestep_input),
+            &mut inner_temp_label,
+            &mut outer_temp_label));
+    app.run().unwrap();
+}
+
+fn parse_float_input(field: &FloatInput) -> f64 {
+    let val: f64 = if field.value().is_empty() {
+        0.0
+    } else {
+        field.value().parse().unwrap_or(0.0)
+    };
+    return val;
 }
 
 #[test]
@@ -95,7 +154,7 @@ fn test_change_temp_simple() {
     assert_eq!(dt, 10.);
 }
 
-fn mass(density: f64, dx: f64) -> f64 {
+fn get_mass(density: f64, dx: f64) -> f64 {
     return density*dx;
 }
 
@@ -141,23 +200,29 @@ fn update_temperatures(ts: &Vec<f64>, net_heat: &Vec<f64>, specific_heat: &f64, 
     return new_temps;
 }
 
-fn iterate(t_initial: f64, dx: f64, k: f64, specific_heat: f64, mass: f64, t_fluid: f64, h: f64, n: i32, duration: f64, dt: f64) -> (Vec<f64>, Vec<f64>, Vec<f64>) {
-    let mut temps = vec![t_initial; n as usize];
+fn run(t_initial: &f64, thickness: &f64, subdiv_count:&f64, k: &f64, specific_heat: &f64, density: &f64, t_fluid: &f64, h: &f64, duration: &f64, dt: &f64, inner_label: &mut Frame, outer_label: &mut Frame){
+    let dx = thickness.clone()/subdiv_count.clone();
+    let n = subdiv_count.clone() as i64;
+    let cell_mass = get_mass(density.clone(), dx);
+    let mut temps = vec![t_initial.clone(); n.clone() as usize];
     let mut outer_temps: Vec<f64> = Vec::with_capacity(temps.len());
     let mut inner_temps: Vec<f64> = Vec::with_capacity(temps.len());
     let mut t = 0.;
 
     outer_temps.push(temps[0]);
-    inner_temps.push(temps[n as usize-1]);
+    inner_temps.push(temps[n.clone() as usize-1]);
 
-    while t <= duration {
+    while t <= duration.clone() {
         let conductions = conductive_flows(&temps, &dx, &k);
         let nets = net_heat_flows(&temps, &conductions, &h, &t_fluid);
-        let new_temps = update_temperatures(&temps, &nets, &specific_heat, &mass, &dt);
+        let new_temps = update_temperatures(&temps, &nets, &specific_heat, &cell_mass, &dt);
         temps = new_temps;
         outer_temps.push(temps[0].clone());
-        inner_temps.push(temps[n as usize-3].clone());
+        inner_temps.push(temps[n.clone() as usize-3].clone());
         t = t + dt;
     }
-    return (temps, outer_temps, inner_temps);
+    let mut inner_temp_string = format!("Inner Wall Temperature: {:.3}",inner_temps[inner_temps.len()-1]);
+    inner_label.set_label(&inner_temp_string);
+    let mut outer_temp_string = format!("Outer Wall Temperature: {:.3}",outer_temps[outer_temps.len()-1]);
+    outer_label.set_label(&outer_temp_string);
 }
