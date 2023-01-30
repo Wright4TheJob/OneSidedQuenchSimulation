fad06f8a0bdd5573f99fd10e5f65567af6df6847
David Wright <djwright3.14@gmail.com>
2023-01-30 06:04:41 -0800

    Added plotting of outputs.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,6 @@
+//use std::slice::range;
+
+use fltk::image::PngImage;
 use fltk::{app, 
     button::Button, 
     frame::Frame, 
@@ -5,6 +8,15 @@ use fltk::{app,
     window::Window, 
     input::FloatInput, 
     group::Flex};
+use plotters::prelude::*;
+use plotters_bitmap::BitMapBackend;
+use std::error::Error;
+
+const W: usize = 400;
+const H: usize = 200;
+//const cells: usize = x_size*y_size*3;
+use std::fs;
+
 
 fn parse_float_input(field: &FloatInput) -> f64 {
     let val: f64 = if field.value().is_empty() {
@@ -131,7 +143,27 @@ fn update_temperatures(ts: &Vec<f64>, net_heat: &Vec<f64>, specific_heat: &f64, 
     return new_temps;
 }
 
-fn run(t_initial: &f64, thickness: &f64, subdiv_count:&f64, k: &f64, specific_heat: &f64, density: &f64, t_fluid: &f64, h: &f64, duration: &f64, dt: &f64, inner_label: &mut Frame, outer_label: &mut Frame){
+fn decimate_vector(xs: &Vec<f64>, target_len: usize) -> Vec<f64> {
+    let current_count = xs.len();
+    let ratio = (current_count+1) / target_len;
+    let indices_to_pull: Vec<usize> = (0..target_len).into_iter().map(|x| x * ratio).collect();
+    indices_to_pull.into_iter().map(|i| xs[i]).collect()
+}
+
+#[test]
+fn test_decimate_vector() {
+    let test_vec = vec![1.0,2.0,3.0,4.0];
+    let small_vec = decimate_vector(&test_vec, 2);
+    assert_eq!(small_vec.len(), 2);
+    assert_eq!(small_vec, vec![1.0,3.0])
+}
+
+fn decimate_temps(temps: &(Vec<f64>, Vec<f64>)) -> (Vec<f64>, Vec<f64>){
+    (decimate_vector(&temps.0, W/2), 
+    decimate_vector(&temps.1, W/2))
+}
+
+fn run(t_initial: &f64, thickness: &f64, subdiv_count:&f64, k: &f64, specific_heat: &f64, density: &f64, t_fluid: &f64, h: &f64, duration: &f64, dt: &f64) -> (Vec<f64>,Vec<f64>){
     let dx = thickness.clone()/subdiv_count.clone();
     let n = subdiv_count.clone() as i64;
     let cell_mass = get_mass(density.clone(), dx);
@@ -152,12 +184,60 @@ fn run(t_initial: &f64, thickness: &f64, subdiv_count:&f64, k: &f64, specific_he
         inner_temps.push(temps[n.clone() as usize-3].clone());
         t = t + dt;
     }
-    let inner_temp_string = format!("Inner Wall Temperature: {:.3}",inner_temps[inner_temps.len()-1]);
-    inner_label.set_label(&inner_temp_string);
-    let outer_temp_string = format!("Outer Wall Temperature: {:.3}",outer_temps[outer_temps.len()-1]);
-    outer_label.set_label(&outer_temp_string);
+    (inner_temps, outer_temps)
 }
 
+fn make_chart(inner_temps: Option<Vec<f64>>, outer_temps:Option<Vec<f64>>, timestep:&f64){
+
+    let drawing_area = BitMapBackend::new("tmp.png", (W as u32, H as u32)).into_drawing_area();
+    drawing_area.fill(&WHITE).unwrap();
+    let mut chart_builder = ChartBuilder::on(&drawing_area);
+    chart_builder.margin(10)
+    .x_label_area_size(30)
+    .y_label_area_size(50);
+    let mut chart_context = match &inner_temps {
+        Some(temps) => {
+            chart_builder.build_cartesian_2d(
+                0.0..temps.len() as f64 * timestep, 
+                0.0..1.05*temps[0]).unwrap()       
+        }, 
+        None => {
+            chart_builder.build_cartesian_2d(
+                0.0..60.0, 
+                0.0..200.0).unwrap()        
+        }
+    };
+    chart_context.configure_mesh()
+    .y_desc("Temperature [C]")
+    .x_desc("Time [seconds]").draw().unwrap();
+    
+    
+    if let Some(temps) = inner_temps{ 
+        chart_context.draw_series(LineSeries::new(
+            (0..).zip(
+                temps.iter()).map(
+                    |(time, temp)|   (time as f64 * timestep, temp.clone())), RED)).unwrap()
+                    .label("Inner Wall")
+                    .legend(|(x, y)| PathElement::new(vec![(x, y), (x + 20, y)], &RED));
+    }
+
+    if let Some(temps) =  outer_temps {
+        chart_context.draw_series(LineSeries::new(
+            (0..).zip(
+                temps.iter()).map(
+                    |(time, temp)|   (time as f64 * timestep, temp.clone())), BLACK)).unwrap()
+                    .label("Outer Wall")
+                    .legend(|(x, y)| PathElement::new(vec![(x, y), (x + 20, y)], &BLACK));    
+        chart_context.configure_series_labels()
+        .border_style(&BLACK)
+        .background_style(&WHITE.mix(0.8))
+        .position(SeriesLabelPosition::UpperRight)
+        .draw()
+        .unwrap();
+    }   
+
+}
+ 
 fn float_input_row(label: &String, pad: &i32) -> FloatInput {
     let mut row = Flex::default_fill().row();
     row.set_pad(pad.clone());
@@ -167,12 +247,20 @@ fn float_input_row(label: &String, pad: &i32) -> FloatInput {
     row.end();
     thickness_input
 }
-fn main() {
+
+fn delete_temp_file() -> std::io::Result<()> {
+    fs::remove_file("tmp.png")?;
+    Ok(())
+}
+
+fn main() -> Result<(), Box<dyn Error>>{
+    //let mut buf = vec![0u8; W * H * 3];
+    //let _ = make_blank_chart(&mut image_data);
     let app = app::App::default();
     let mut wind = Window::default().with_size(500, 800).with_label("Quenching Temperatures");
     wind.make_resizable(true);
 
-    let pad = 20;
+    let pad = 5;
     let mut main_column = Flex::default_fill()
         .column();
     main_column.set_margin(pad.clone());
@@ -216,10 +304,29 @@ fn main() {
 
     let mut button_start = Button::default()
         .with_label("Run Simulation");
+
+    let result_row = Flex::default_fill().row();
+
     let mut inner_temp_label = Frame::default().with_label("");
     let mut outer_temp_label = Frame::default().with_label("");
+    result_row.end();
+    
+    //let mut image_data: [u8; W*H*3] = [0; W*H*3];
+    let _ = make_chart(None, None, &1.0); 
+    //println!("make_blanke_chart result: {:?}", res);
+    let chart = PngImage::load("tmp.png").expect("Failed to load PNG from disk");
+    let mut chart_frame = Frame::default();
+    chart_frame.set_image(Some(chart));
+    //println!("{:?}",chart_res);
+
+    main_column.set_size(&chart_frame, H as i32);
+
     main_column.end();
 
+    wind.set_callback(|this_window| {
+        let _=delete_temp_file();
+        this_window.hide();
+    });
     wind.end();
     wind.show();
 
@@ -229,8 +336,8 @@ fn main() {
     let density = 7850.; // kg/m^3
 
     /* Event handling */
-    button_start.set_callback(move |_|
-        run(&parse_float_input(&initial_temp_input),
+    button_start.set_callback(move |_| {
+        let temps = run(&parse_float_input(&initial_temp_input),
             &(parse_float_input(&thickness_input)/1000.),
             &parse_float_input(&material_subdivs_input),
             &material_k,
@@ -239,8 +346,26 @@ fn main() {
             &parse_float_input(&fluid_temp_input),
             &parse_float_input(&convective_input),
             &parse_float_input(&duration_input),
-            &parse_float_input(&timestep_input),
-            &mut inner_temp_label,
-            &mut outer_temp_label));
+            &parse_float_input(&timestep_input));
+        
+        let inner_temps = &temps.0;
+        let outer_temps = &temps.1;
+        let inner_temp_string = format!("Inner Wall Temperature: {:.3}",inner_temps[inner_temps.len()-1]);
+        inner_temp_label.set_label(&inner_temp_string);
+        let outer_temp_string = format!("Outer Wall Temperature: {:.3}",outer_temps[outer_temps.len()-1]);
+        outer_temp_label.set_label(&outer_temp_string);
+
+        let sparse_temps = decimate_temps(&temps);
+        let sparse_timestep = &parse_float_input(&duration_input) /(W/2) as f64;
+        let _ = make_chart(
+            Some(sparse_temps.0), 
+            Some(sparse_temps.1),
+            &sparse_timestep);
+
+        let chart = PngImage::load("tmp.png").expect("Failed to load PNG from disk");
+        chart_frame.set_image(Some(chart));
+        chart_frame.redraw();    
+    });
     app.run().unwrap();
+Ok(())
 }
